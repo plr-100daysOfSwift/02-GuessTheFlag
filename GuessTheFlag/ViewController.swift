f5bae03378e1cc68126e3f4c14b8b50006c03ccf
Paul Richardson <developer@photographerafoot.com>
2021-04-02 09:05:17 +1100

    CHG: add a variable to record the number of attempts

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
 	var countries = [String]()
 	var score = 0
 	var correctAnswer = 0
+	var tries = 0
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
