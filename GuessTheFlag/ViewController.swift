db1c2280b1f7568c36dd0388b45809587162177a
Paul Richardson <developer@photographerafoot.com>
2021-04-02 08:53:12 +1100

    CHG: update the label text

--- GuessTheFlag/ViewController.swift
+++ GuessTheFlag/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
 			button.layer.borderWidth = 1
 			button.layer.borderColor = UIColor.lightGray.cgColor
 		}
-
+		scoreLabel.text = "Your score is \(score)."
 		askQuestion()
 	}
 
@@ -47,6 +47,7 @@ class ViewController: UIViewController {
 			title = "Wrong"
 			score -= 1
 		}
+		scoreLabel.text = "Your score is \(score)."
 
 		let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
 		ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
